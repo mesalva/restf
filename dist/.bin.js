a0eb6d18a43e20e747f73e97b7ec45007846626b
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-02-06 10:30:13 -0300

    Fix development

--- dist/.bin.js
+++ dist/.bin.js
@@ -4,6 +4,8 @@ var fs = require("fs");
 function declareControllers(folderPath, repoPath) {
     if (folderPath === void 0) { folderPath = 'src'; }
     if (repoPath === void 0) { repoPath = undefined; }
+    if (!repoPath)
+        repoPath = process.cwd();
     var controllersFolderPath = process.cwd() + "/" + folderPath + "/controllers";
     if (!fs.existsSync(controllersFolderPath)) {
         return fs.writeFileSync(repoPath + "/node_modules/restf/.allControllers.js", 'module.exports = {}');
@@ -14,7 +16,9 @@ function declareControllers(folderPath, repoPath) {
         .map(function (file) { return file.replace(/\.[tj]s$/, ''); });
     var content = 'module.exports = {\n';
     content += files
-        .map(function (controllerName) { return "  " + controllerName + ": require('" + repoPath + "/" + folderPath + "/controllers/" + controllerName + "').default,"; })
+        .map(function (controllerName) {
+        return "  " + controllerName + ": require('" + repoPath + "/" + folderPath + "/controllers/" + controllerName + "').default,";
+    })
         .join('\n');
     content += '\n}';
     fs.writeFileSync(process.cwd() + "/node_modules/restf/.allControllers.js", content);
--- src/.bin.ts
+++ src/.bin.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs'
 
 export function declareControllers(folderPath = 'src', repoPath = undefined) {
+  if (!repoPath) repoPath = process.cwd()
   const controllersFolderPath = `${process.cwd()}/${folderPath}/controllers`
   if (!fs.existsSync(controllersFolderPath)) {
     return fs.writeFileSync(`${repoPath}/node_modules/restf/.allControllers.js`, 'module.exports = {}')
@@ -11,7 +12,10 @@ export function declareControllers(folderPath = 'src', repoPath = undefined) {
     .map(file => file.replace(/\.[tj]s$/, ''))
   let content = 'module.exports = {\n'
   content += files
-    .map(controllerName => `  ${controllerName}: require('${repoPath}/${folderPath}/controllers/${controllerName}').default,`)
+    .map(
+      controllerName =>
+        `  ${controllerName}: require('${repoPath}/${folderPath}/controllers/${controllerName}').default,`
+    )
     .join('\n')
   content += '\n}'
   fs.writeFileSync(`${process.cwd()}/node_modules/restf/.allControllers.js`, content)
