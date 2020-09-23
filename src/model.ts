f80b1a5d35acb9695dee644711ef2b32b26695ad
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-09-23 15:59:03 -0300

    refactor: model

--- src/model.ts
+++ src/model.ts
@@ -1,27 +1,35 @@
 import _database from './_database'
+import { Client } from 'pg'
 
 export default class RestfModel {
-  db: any
+  _db: any
 
-  constructor(protected table: string) {
-    this.db = _database(table)
-    this.db.constructor.prototype.raw = async (query: string, options: any[] = []) => {
+  protected route?: string
+
+  constructor(protected options?: string | any) {}
+
+  protected get db(){
+    if(this._db) return this._db
+    let table: string = typeof this.options === 'string' ? this.options : ''
+    if(!this.options) table = this.route
+    this._db = _database(table)
+    this._db.constructor.prototype.raw = async (query: string, options: any[] = []) => {
       return _database.raw(query.replace(/\n/g, ' '), options).then(({ rows }) => rows)
     }
-    this.db.constructor.prototype.firstParsed = (...args) => {
-      return this.db.first(...args).then(parseSingle)
+    this._db.constructor.prototype.firstParsed = (...args) => {
+      return this._db.first(...args).then(parseSingle)
     }
-    this.db.constructor.prototype.selectParsed = (...args) => {
-      return this.db.select(...args).then(parseMulti)
+    this._db.constructor.prototype.selectParsed = (...args) => {
+      return this._db.select(...args).then(parseMulti)
     }
-    this.db.constructor.prototype.innerJoinWithManyAnds = (table, ...args) => {
-      return this.db.innerJoin(table, function() {
+    this._db.constructor.prototype.innerJoinWithManyAnds = (table, ...args) => {
+      return this._db.innerJoin(table, function() {
         const [a, b, c, d] = args
         this.on(a, '=', b).andOn(c, '=', d)
       })
     }
-    this.db.constructor.insertReturning = (content, ...returns) => {
-      return this.db
+    this._db.constructor.insertReturning = (content, ...returns) => {
+      return this._db
         .insert(content)
         .returning('id')
         .then(([id]) => id)
@@ -31,28 +39,48 @@ export default class RestfModel {
             .first(...returns)
         )
     }
+    return this._db
+  }
+
+  protected async rawSelect(query: string, params: any[] = []) {
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL + '?ssl=true',
+    })
+    await client.connect()
+    if (query.toLowerCase().substr(0, 6) !== 'select') {
+      throw new Error('Only selects are allowed')
+    }
+    const res = await client.query(query, [...params])
+    client.end()
+    return res.rows
+  }
+
+  protected async rawSelectFirst(query: string, params: any[]) {
+    const result = await this.rawSelect(query, params)
+    if (result && Array.isArray(result) && result.length > 0) return result[0]
+    return null
   }
 
   all() {
-    return this.db.select()
+    return this._db.select()
   }
 
   find(id: number) {
-    return this.db
+    return this._db
       .first()
       .where({ id })
       .then((data: any) => (data ? data : Promise.reject(new Error('Content Not Found'))))
   }
 
   findBy(column: string, value: any) {
-    return this.db
+    return this._db
       .first()
       .where({ [column]: value })
       .then((data: any) => (data ? data : Promise.reject(new Error('Content Not Found'))))
   }
 
   insertGetId(data: any) {
-    return this.db
+    return this._db
       .insert(data)
       .returning('id')
       .then((ids: Array<number>) => ({ id: ids[0] }))
@@ -63,13 +91,13 @@ export default class RestfModel {
   }
 
   update(data: object, whereClause?: any) {
-    const db = this.db.update(data)
+    const db = this._db.update(data)
     if (!whereClause) return db
     return db.where(whereClause)
   }
 
   delete(id: number) {
-    return this.db
+    return this._db
       .where({ id })
       .delete()
       .then(() => ({ id }))
